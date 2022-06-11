314a88e193fdf7fcdca6a5ea7e3eb65702c791e8
Darren <darren@omnichat.ai>
2022-06-11 11:04:15 +0800

    add some comments

--- iterator/components/browserdetection.js
+++ iterator/components/browserdetection.js
@@ -1,7 +1,9 @@
+// The function will randonly generate a boolean.
 const randomBooleanGenerator = () => {
     return Math.floor(Math.random() * 2) == 0
 }
 
+// The object containes the functions which detect if a bowser belongs to the designated one.
 const browserDetection = {
     edge: () => { 
         return randomBooleanGenerator()
